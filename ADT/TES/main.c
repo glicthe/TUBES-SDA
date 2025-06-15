2b5099fe65ad8cfe6aebd2204a09c9499923ebb8
Rangga <rangga.m.fajar99@gmail.com>
2025-06-16 02:56:21 +0700

    Revert "solve bug in secondary boss"
    This reverts commit 4fa49622984ba5f26a4e4c4d60b03d38ccfaef6a.

--- ADT/TES/main.c
+++
@@ -1,9 +0,0 @@
-#include "menu.h"
-
-int main() {
-//	SetConsoleSettings();
-//    HWND hWnd=GetConsoleWindowNT();
-//    MoveWindow(hWnd, 0, 0, 932, 570,TRUE);
-    SplashScreen();
-    return 0;
-}
--- LIEUR/main.c
+++
@@ -1,6 +0,0 @@
-#include "menu.h"
-
-int main() {
-    SplashScreen();
-    return 0;
-}
